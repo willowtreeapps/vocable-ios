303dc5cd53982c9e2a8a986162e4812c38879ba0
Kyle Ohanian <kyle.ohanian@willowtreeapps.com>
2019-06-26 13:39:46 -0400

    other stuff

---
+++ prototypes/EyeTrackingTest/EyeTrackingTest/Gestures/BlinkGesture.swift
@@ -0,0 +1,29 @@
+//
+//  BlinkGesture.swift
+//  EyeTrackingTest
+//
+//  Created by Kyle Ohanian on 6/26/19.
+//  Copyright © 2019 WillowTree. All rights reserved.
+//
+
+import Foundation
+import ARKit
+
+class BlinkGesture: FaceGesture {
+    private var gestureType: ARFaceAnchor.BlendShapeLocation
+    private var eyeState: EyeState = .open
+    
+    init(gestureType: ARFaceAnchor.BlendShapeLocation, requiredGestures: Int) {
+        self.gestureType = gestureType
+        super.init()
+        self.requiredGestures = requiredGestures
+    }
+    
+    override func prepare(withAnchor anchor: ARFaceAnchor) {
+        self.isGestureDetected = false
+        if let value = anchor.blendShapes[gestureType] {
+            let didUpdate = self.eyeState.update(value: value)
+            self.isGestureDetected = didUpdate && self.eyeState == .open
+        }
+    }
+}
