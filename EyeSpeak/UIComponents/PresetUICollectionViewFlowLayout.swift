9244f866cc1b726544e5d9cd7fe0be42e2fd81b8
Jesse Morgan <jesse.morgan@willowtreeapps.com>
2020-02-04 17:36:59 -0500

    Subclassed UICollectionViewCompositionalLayout for custom animations

---
+++ EyeSpeak/UIComponents/PresetUICollectionViewFlowLayout.swift
@@ -0,0 +1,24 @@
+//
+//  PresetUICollectionViewFlowLayout.swift
+//  EyeSpeak
+//
+//  Created by Jesse Morgan on 2/4/20.
+//  Copyright © 2020 WillowTree. All rights reserved.
+//
+
+import UIKit
+
+class PresetUICollectionViewCompositionalLayout: UICollectionViewCompositionalLayout {
+    
+    override func initialLayoutAttributesForAppearingItem(at itemIndexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        let attr = super.initialLayoutAttributesForAppearingItem(at: itemIndexPath)
+        attr?.transform = CGAffineTransform(translationX: 0, y: 500.0)
+        return attr
+    }
+    
+    override func finalLayoutAttributesForDisappearingItem(at itemIndexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        let attr = super.initialLayoutAttributesForAppearingItem(at: itemIndexPath)
+        attr?.transform = CGAffineTransform(translationX: 0, y: -500.0)
+        return attr
+    }
+}
