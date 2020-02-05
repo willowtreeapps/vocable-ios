b095fc1038305c91f9d65118d9cde56c793c6399
Jesse Morgan <jesse.morgan@willowtreeapps.com>
2020-02-05 14:23:36 -0500

    Updating  category background color selection

--- EyeSpeak/TextSelection/TextSelectionViewController.swift
+++ EyeSpeak/TextSelection/TextSelectionViewController.swift
@@ -103,6 +103,7 @@ class TextSelectionViewController: UICollectionViewController {
         collectionView.register(UINib(nibName: "TrackingButtonCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "TrackingButtonCollectionViewCell")
         collectionView.collectionViewLayout = createLayout()
         collectionView.backgroundColor = .black
+        collectionView.allowsMultipleSelection = true
         
         collectionView.register(PresetPageControlReusableView.self, forSupplementaryViewOfKind: "footerPageIndicator", withReuseIdentifier: "PresetPageControlView")
         
@@ -274,6 +275,13 @@ class TextSelectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let selectedItem = dataSource.itemIdentifier(for: indexPath) else { return }
+        
+        for selectedPath in collectionView.indexPathsForSelectedItems ?? [] {
+            if selectedPath.section == indexPath.section && selectedPath != indexPath {
+                collectionView.deselectItem(at: selectedPath, animated: true)
+            }
+        }
+        
         switch selectedItem {
         case .presetItem(let text):
             let utterance = AVSpeechUtterance(string: text)
@@ -286,9 +294,11 @@ class TextSelectionViewController: UICollectionViewController {
             currentSpeechText = text
         case .category(let category):
             selectedCategory = category
+            return
         default:
             break
         }
+        
         if collectionView.indexPathForGazedItem != indexPath {
             collectionView.deselectItem(at: indexPath, animated: true)
         }
