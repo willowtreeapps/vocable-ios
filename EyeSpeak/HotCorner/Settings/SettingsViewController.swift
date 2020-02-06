e4fb059be69a7860affe5ea9dd5c99e43a4e0c4b
Jesse Morgan <jesse.morgan@willowtreeapps.com>
2020-02-06 18:51:14 -0500

    Adding hot corner button that will show settings page

---
+++ EyeSpeak/HotCorner/Settings/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  EyeSpeak
+//
+//  Created by Jesse Morgan on 2/6/20.
+//  Copyright © 2020 WillowTree. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.backgroundColor = .collectionViewBackgroundColor
+    }
+    
+}
