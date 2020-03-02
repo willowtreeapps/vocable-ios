61ff1d06f6160ed9db919c141741f4d649ed64f5
Thomas Shealy <thomasshealy32@gmail.com>
2020-03-02 15:55:32 -0500

    Finished adding an alert which appears at the top of the screen when the user is out of head tracking range.

--- EyeSpeak/Common/Views/TrackingContainerViewController.swift
+++ EyeSpeak/Common/Views/TrackingContainerViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 final class TrackingContainerViewController: UIViewController {
 
+    @IBOutlet weak var warningView: UIView!
     @IBOutlet private var cursorView: UIVirtualCursorView!
     
     private var contentViewController: UIViewController!
+    
+    override func viewDidLoad() {
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidAcquireGaze(_:)), name: .applicationDidAcquireGaze, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidLoseGaze(_:)), name: .applicationDidLoseGaze, object: nil)
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ContentViewControllerSegue" {
@@ -22,5 +28,17 @@ final class TrackingContainerViewController: UIViewController {
     
     override var childForHomeIndicatorAutoHidden: UIViewController? {
         return contentViewController
+    }
+    
+    @objc private func applicationDidAcquireGaze(_ sender: Any?) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.warningView.alpha = 0.0
+        })
+    }
+
+    @objc private func applicationDidLoseGaze(_ sender: Any?) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.warningView.alpha = 1.0
+        })
     }
 }
--- EyeSpeak/Features/Settings/SettingsViewController.swift
+++ EyeSpeak/Features/Settings/SettingsViewController.swift
@@ -12,9 +12,30 @@ import UIKit
 class SettingsViewController: UIViewController {
     
     @IBOutlet var dismissButton: GazeableButton!
-    
+
     @IBAction func dismissSettings(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    @IBOutlet weak var warningView: UIView!
+    
+    override func viewDidLoad() {
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidAcquireGaze(_:)), name: .applicationDidAcquireGaze, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidLoseGaze(_:)), name: .applicationDidLoseGaze, object: nil)
+        if !UIApplication.shared.isGazeTrackingActive {
+            warningView.alpha = 1.0
+        }
+    }
+    
+    @objc private func applicationDidAcquireGaze(_ sender: Any?) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.warningView.alpha = 0.0
+        })
+    }
+
+    @objc private func applicationDidLoseGaze(_ sender: Any?) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.warningView.alpha = 1.0
+        })
     }
     
 }
