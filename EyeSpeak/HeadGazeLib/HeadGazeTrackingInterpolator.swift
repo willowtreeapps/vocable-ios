c0ca275e1e4614f03b811dce352237e557768ee0
Steve Foster <steve.foster@willowtreeapps.com>
2020-03-06 14:03:38 -0500

    Tweaks to iphone head tracking

--- EyeSpeak/HeadGazeLib/HeadGazeTrackingInterpolator.swift
+++ EyeSpeak/HeadGazeLib/HeadGazeTrackingInterpolator.swift
@@ -107,6 +107,7 @@ class HeadGazeTrackingInterpolator {
 
         let xNDC = Float(hitPos[0]) - Float(adjustedCorrection.width)
         let yNDC = Float(hitPos[1]) - Float(adjustedCorrection.height)
+
         let hitPosNDC = SIMD2<Float>([xNDC, yNDC])
         let filteredPos: SIMD2<Float>
         filteredPos = interpolateNDCLocation(hitPosNDC)
