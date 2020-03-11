f399291f40ad3b9068a82fa6886957547723d408
Steve Foster <steve.foster@willowtreeapps.com>
2020-03-11 11:37:18 -0400

    Renamed Test directory

--- VocableTests/VocableTests.swift
+++ VocableTests/VocableTests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class EyeSpeakTests: XCTestCase {
+class VocableTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
