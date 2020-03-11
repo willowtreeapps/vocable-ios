7aa16ef269e43636483f12183a193b29412a428a
Thomas Shealy <thomasshealy32@gmail.com>
2020-03-11 14:34:55 -0400

    UI Work

---
+++ Vocable/Features/Settings/EditSayings/EditSayingsCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EditSayingsCollectionViewController.swift
+//  Vocable
+//
+//  Created by Thomas Shealy on 3/11/20.
+//  Copyright © 2020 WillowTree. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class EditSayingsCollectionViewController: UICollectionViewController {
+    
+}
---
+++ Vocable/Features/Settings/EditSayings/EditSayingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EditSayingsViewController.swift
+//  Vocable
+//
+//  Created by Thomas Shealy on 3/11/20.
+//  Copyright © 2020 WillowTree. All rights reserved.
+//
+
+import UIKit
+
+class EditSayingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
