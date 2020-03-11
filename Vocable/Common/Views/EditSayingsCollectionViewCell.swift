290d4029cf390ba2da5f01994934c0dcd2e5b861
Thomas Shealy <thomasshealy32@gmail.com>
2020-03-11 13:34:54 -0400

    UI Work

---
+++ Vocable/Common/Views/EditSayingsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EditSayingsCollectionViewCell.swift
+//  EyeSpeak
+//
+//  Created by Thomas Shealy on 3/11/20.
+//  Copyright © 2020 WillowTree. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditSayingsCollectionViewCell: VocableCollectionViewCell {
+    
+}
