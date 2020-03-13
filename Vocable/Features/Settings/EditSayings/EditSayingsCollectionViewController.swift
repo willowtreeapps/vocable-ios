4d66bfd29e0e233e42354c4652819137d68f3efc
Jesse Morgan <jesse.morgan@willowtreeapps.com>
2020-03-13 15:46:11 -0400

    Creating EditSayingsCollectionViewController

---
+++ Vocable/Features/Settings/EditSayings/EditSayingsCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EditSayingsCollectionViewController.swift
+//  Vocable
+//
+//  Created by Jesse Morgan on 3/13/20.
+//  Copyright © 2020 WillowTree. All rights reserved.
+//
+
+import Foundation
+
+class EditSayingsCollectionViewController: CarouselGridCollectionViewController {
+    
+}
