92c7f97eac3ac2ba6dfc6721777a28aa6877b112
Jesse Morgan <jesse.morgan@willowtreeapps.com>
2020-03-31 15:59:49 -0400

    Implementing basic UI for editing categories

---
+++ Vocable/Features/Settings/EditCategories/EditCategoriesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EditCategoriesViewController.swift
+//  Vocable
+//
+//  Created by Jesse Morgan on 3/31/20.
+//  Copyright © 2020 WillowTree. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditCategoriesViewController: UIViewController {
+    
+}
