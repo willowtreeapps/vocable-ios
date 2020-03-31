a30dc9c9917707c1b86f1b11b7cdac5f0b7991b7
Thomas Shealy <thomasshealy32@gmail.com>
2020-03-31 19:51:33 -0400

    Finished up the primary UI work on the edit categories screen.

--- Vocable/Features/Settings/EditCategories/EditCategoriesViewController.swift
+++ Vocable/Features/Settings/EditCategories/EditCategoriesViewController.swift
@@ -8,7 +8,44 @@
 
 import Foundation
 import UIKit
+import Combine
 
 class EditCategoriesViewController: UIViewController {
+    
+    @IBOutlet weak var paginationView: PaginationView!
+    
+    private var carouselCollectionViewController: CarouselGridCollectionViewController?
+    private var disposables = Set<AnyCancellable>()
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "CarouselCollectionViewController" {
+           carouselCollectionViewController = segue.destination as? CarouselGridCollectionViewController
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       carouselCollectionViewController?.progressPublisher.sink(receiveValue: { (pagingProgress) in
+            guard let pagingProgress = pagingProgress else {
+                return
+            }
+            if pagingProgress.pageCount > 1 {
+                self.paginationView.setPaginationButtonsEnabled(true)
+            } else {
+                self.paginationView.setPaginationButtonsEnabled(false)
+            }
+            let computedPageCount = max(pagingProgress.pageCount, 1)
+
+            self.paginationView.textLabel.text = String(format: NSLocalizedString("Page %d of %d", comment: ""), pagingProgress.pageIndex + 1, computedPageCount)
+        }).store(in: &disposables)
+        
+        paginationView.nextPageButton.addTarget(carouselCollectionViewController, action: #selector(CarouselGridCollectionViewController.scrollToNextPage), for: .primaryActionTriggered)
+        paginationView.previousPageButton.addTarget(carouselCollectionViewController, action: #selector(CarouselGridCollectionViewController.scrollToPreviousPage), for: .primaryActionTriggered)
+        
+    }
+    
+    @IBAction func backToEditCategories(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
     
 }
---
+++ Vocable/Features/Settings/EditCategories/Views/EditCategoryToggleCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EditCategoryToggleCollectionViewCell.swift
+//  Vocable
+//
+//  Created by Thomas Shealy on 3/31/20.
+//  Copyright © 2020 WillowTree. All rights reserved.
+//
+
+import UIKit
+
+class EditCategoryToggleCollectionViewCell: VocableCollectionViewCell {
+    
+    @IBOutlet weak var showCategorySwitch: UISwitch!
+    @IBOutlet weak var showCategoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        showCategorySwitch.isEnabled = true
+        showCategoryLabel.text = NSLocalizedString("Show", comment: "Show category toggle label.")
+        
+    }
+}
