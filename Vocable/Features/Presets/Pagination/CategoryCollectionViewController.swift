d5e0874455c8ebfd1da92e3219430edd9c157239
Jesse Morgan <jesse.morgan@willowtreeapps.com>
2020-04-06 14:16:32 -0400

    Refactored view models to use fetch results controller

--- Vocable/Features/Presets/Pagination/CategoryCollectionViewController.swift
+++ Vocable/Features/Presets/Pagination/CategoryCollectionViewController.swift
@@ -11,25 +11,21 @@ import UIKit
 
 class CategoryCollectionViewController: CarouselGridCollectionViewController, NSFetchedResultsControllerDelegate {
     
-    private lazy var categoryViewModels: [CategoryViewModel] =
-    Category.fetchAll(in: NSPersistentContainer.shared.viewContext,
-                      sortDescriptors: [NSSortDescriptor(keyPath: \Category.identifier, ascending: true)])
-        .compactMap { CategoryViewModel($0) }
-    
-    private lazy var dataSource = UICollectionViewDiffableDataSource<Int, CategoryViewModel>(collectionView: collectionView!) { [weak self] (collectionView, indexPath, _) -> UICollectionViewCell? in
+    private lazy var dataSource = UICollectionViewDiffableDataSource<Int, Category>(collectionView: collectionView!) { [weak self] (collectionView, indexPath, category) -> UICollectionViewCell? in
         guard let self = self else { return nil }
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryItemCollectionViewCell.reuseIdentifier, for: indexPath) as! CategoryItemCollectionViewCell
-        cell.setup(title: self.categoryViewModels[indexPath.row].name)
+        cell.setup(title: category.name!)
         return cell
     }
 
     private lazy var fetchRequest: NSFetchRequest<Category> = {
         let request: NSFetchRequest<Category> = Category.fetchRequest()
-        request.sortDescriptors = [NSSortDescriptor(keyPath: \Phrase.creationDate, ascending: false)]
+        request.predicate = NSComparisonPredicate(\Category.isHidden, .equalTo, false)
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \Category.ordinal, ascending: true)]
         return request
     }()
 
-    private lazy var fetchResultsController = NSFetchedResultsController<Phrase>(fetchRequest: self.fetchRequest,
+    private lazy var fetchResultsController = NSFetchedResultsController<Category>(fetchRequest: self.fetchRequest,
                                                                                  managedObjectContext: NSPersistentContainer.shared.viewContext,
                                                                                  sectionNameKeyPath: nil,
                                                                                  cacheName: nil)
@@ -70,7 +66,6 @@ class CategoryCollectionViewController: CarouselGridCollectionViewController, NS
         }
     }
 
-
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         updateDataSource(animated: true, completion: { [weak self] in
             self?.layout.resetScrollViewOffset(inResponseToUserInteraction: false,
@@ -80,17 +75,16 @@ class CategoryCollectionViewController: CarouselGridCollectionViewController, NS
 
     private func updateDataSource(animated: Bool, completion: (() -> Void)? = nil) {
         let content = fetchResultsController.fetchedObjects ?? []
-        let viewModels = content.compactMap(PhraseViewModel.init)
-        var snapshot = NSDiffableDataSourceSnapshot<Int, CategoryViewModel>()
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Category>()
         snapshot.appendSections([0])
-        snapshot.appendItems(categoryViewModels)
+        snapshot.appendItems(content)
         dataSource.apply(snapshot,
                                  animatingDifferences: animated,
                                  completion: completion)
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        ItemSelection.selectedCategory = categoryViewModels[indexPath.row]
+        ItemSelection.selectedCategory = CategoryViewModel(fetchResultsController.object(at: indexPath))!
     }
 
 }
