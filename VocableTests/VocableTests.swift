7f9c6f43ac1b7b7438827dd2d95b1d4d809871b2
Kevin Stechler <kevin.stechler@willowtreeapps.com>
2020-04-23 13:27:14 -0400

    removing test unit test

--- VocableTests/VocableTests.swift
+++ VocableTests/VocableTests.swift
@@ -18,7 +18,7 @@ class VocableTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func skip_testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
