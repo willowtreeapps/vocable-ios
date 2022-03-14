43b0b465bc2d98ed3f29e790e9246fe5e0939c29
rudysalas <rudy.salas@willowtreeapps.com>
2022-03-14 17:03:22 -0400

    Corrected typos

--- VocableUITests/Screens/BaseScreen.swift
+++ VocableUITests/Screens/BaseScreen.swift
@@ -16,25 +16,25 @@ class BaseScreen {
     let paginationLabel = XCUIApplication().staticTexts["bottomPagination.pageNumber"]
     
     /**
-     This method extracts a substring from  text, that matches the named group within a defined regex pattern.
+     This method extracts a substring from text, that matches the named group within a defined regex pattern.
      
      For regex pattern #"Find (?\<groupdName\>\w)+ please"#, the \<groupName\> is matched from 'Find help please'
      to return 'help'
      
      Documentaion: https://nshipster.com/swift-regular-expressions/
     */
-    private func getNamedGroupInRegexPatternFromText(namedGroup: String, regexPattern: String, fromText: String) -> String {
+    private func getNamedGroupInRegexPatternFromText(namedGroup: String, regexPattern: String, text: String) -> String {
         var matchingText = ""
         let regex = try! NSRegularExpression(pattern: regexPattern)
-        let range = NSRange(location: 0, length: fromText.count) // Used for the NSRegularExpression
+        let range = NSRange(location: 0, length: text.count) // Used for the NSRegularExpression
         
         // Find a match to the regex pattern...
-        if let match = regex.firstMatch(in: fromText, options: [], range: range) {
+        if let match = regex.firstMatch(in: text, options: [], range: range) {
             // Within the match to the regex pattern, extract the matching group by its name
             let resultingMatch = match.range(withName: namedGroup)
 
             // Extract the Substring (as a String) of the found match from the range returned by regex.firstMatch
-            matchingText = String(fromText[Range(resultingMatch, in: fromText)!])
+            matchingText = String(text[Range(resultingMatch, in: text)!])
         }
         
         return matchingText
@@ -46,7 +46,7 @@ class BaseScreen {
         // Define a regex pattern with named matching group, 'current', for reference
         let pattern = #"(?<current>\d)+ of (\d)+"#
         
-        return getNamedGroupInRegexPatternFromText(namedGroup: "current", regexPattern: pattern, fromText: paginationLabel.label)
+        return getNamedGroupInRegexPatternFromText(namedGroup: "current", regexPattern: pattern, text: paginationLabel.label)
     }
     
     /// For Pagination: retrieve the total number of pages (Y) from the
@@ -55,7 +55,7 @@ class BaseScreen {
         // Define a regex pattern with named matching group, 'total', for reference
         let pattern = #"(\d)+ of (?<total>\d)+"#
         
-        return getNamedGroupInRegexPatternFromText(namedGroup: "total", regexPattern: pattern, fromText: paginationLabel.label)
+        return getNamedGroupInRegexPatternFromText(namedGroup: "total", regexPattern: pattern, text: paginationLabel.label)
     }
     
 }
