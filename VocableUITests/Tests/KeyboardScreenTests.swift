fba398db8a5c00f8143e692028817b553bb0d1da
CananArikan <canan.arikan@willowtreeapps.com>
2022-03-17 16:06:11 -0400

    Fixed Keyboard Screen Tests
    testKeyboardOutputIsDisplayed is fixed. testAddPhraseToMySayingsFromKeyboard and testRemovePhraseFromMySayingsFromKeyboard are added.

--- VocableUITests/Tests/KeyboardScreenTests.swift
+++ VocableUITests/Tests/KeyboardScreenTests.swift
@@ -3,30 +3,59 @@
 //  KeyboardScreenTests
 //
 //  Created by Kevin Stechler on 4/22/20.
-//  Copyright © 2020 WillowTree. All rights reserved.
+//  Updated by Rudy Salas and Canan Arikan on 03/17/2022
+//  Copyright © 2022 WillowTree. All rights reserved.
 //
 
 import XCTest
 
 class KeyboardScreenTests: BaseTest {
-    let testPhrase = "Test"
-
-    func testWhenTyping_ThenTextShownOnOutputLabel() {
+    
+    func testKeyboardOutputIsDisplayed() {
+        let testPhrase = "Test" + randomString(length: 2)
+        
         mainScreen.keyboardNavButton.tap()
         keyboardScreen.typeText(testPhrase)
        
-        XCTAssert(keyboardScreen.keyboardTextView.staticTexts[testPhrase].exists, "Expected the text \(testPhrase) to be displayed")
+        XCTAssertTrue(keyboardScreen.keyboardTextView.staticTexts[testPhrase].exists, "Expected the text \(testPhrase) to be displayed")
     }
-    /*
-     // My sayings is being replaced with Custom Categories so this test will no longer apply.
-    func testWhenAddingPhraseToMySayings_ThenItAppearsOnMainScreen() {
+  
+    func testAddPhraseToMySayingsFromKeyboard() {
+        let testPhrase = "Test" + randomString(length: 2)
+        
         mainScreen.keyboardNavButton.tap()
         keyboardScreen.typeText(testPhrase)
         keyboardScreen.favoriteButton.tap()
         keyboardScreen.dismissKeyboardButton.tap()
-        mainScreen.scrollLeftAndTapCurrentCategory(numTimesToScroll: 1)
-
-        XCTAssert(mainScreen.isTextDisplayed(testPhrase), "Expected the phrase \(testPhrase) to be added to an displayed in 'My Sayings'")
+        
+        mainScreen.scrollToMySayings()
+        mainScreen.findPhrase(phrase: testPhrase)
+        
+        XCTAssertTrue(mainScreen.isTextDisplayed(testPhrase), "Expected the phrase \(testPhrase) to be added to an displayed in 'My Sayings'")
     }
-    */
+    
+    func testRemovePhraseFromMySayingsFromKeyboard() {
+       let testPhrase = "Test" + randomString(length: 2)
+       
+        mainScreen.keyboardNavButton.tap()
+        keyboardScreen.typeText(testPhrase)
+        keyboardScreen.favoriteButton.tap()
+        keyboardScreen.dismissKeyboardButton.tap()
+       
+        mainScreen.scrollToMySayings()
+        mainScreen.findPhrase(phrase: testPhrase)
+       
+        XCTAssertTrue(mainScreen.isTextDisplayed(testPhrase), "Expected the phrase \(testPhrase) to be added to an displayed in 'My Sayings'")
+        
+        mainScreen.keyboardNavButton.tap()
+        keyboardScreen.typeText(testPhrase)
+        keyboardScreen.favoriteButton.tap()
+        keyboardScreen.dismissKeyboardButton.tap()
+        mainScreen.scrollToMySayings()
+         
+        XCTAssertFalse(mainScreen.isTextDisplayed(testPhrase), "Expected the phrase \(testPhrase) to be deleted from 'My Sayings'")
+    }
+    
+
+    
 }
