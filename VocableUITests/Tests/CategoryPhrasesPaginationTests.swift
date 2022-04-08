877df3a29925ead8ef1da1f8d7d8d001dc3dbe6d
rudysalas <rhsalas3@gmail.com>
2022-04-08 16:24:06 -0400

    Created CategoryPhrasePaginationTests and MainScreenPaginationTests

---
+++ VocableUITests/Tests/CategoryPhrasesPaginationTests.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryPhrasesPaginationTests.swift
+//  VocableUITests
+//
+//  Created by Rudy Salas on 4/8/22.
+//  Copyright © 2022 WillowTree. All rights reserved.
+//
+
+import XCTest
+
+class CategoryPhrasesPaginationTests: BaseTest {
+
+    
+}
---
+++ VocableUITests/Tests/MainScreenPaginationTests.swift
@@ -0,0 +1,14 @@
+//
+//  MainScreenPaginationTests.swift
+//  VocableUITests
+//
+//  Created by Rudy Salas on 4/8/22.
+//  Copyright © 2022 WillowTree. All rights reserved.
+//
+
+import XCTest
+
+class MainScreenPaginationTests: BaseTest {
+
+    
+}
