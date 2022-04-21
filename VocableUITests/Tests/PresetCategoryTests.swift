423e55164d60dbfe5b7f0e315f114608f602e7ac
CananArikan <canan.arikan@willowtreeapps.com>
2022-04-21 11:02:39 -0400

    Updated with develop

--- VocableUITests/Tests/PresetCategoryTests.swift
+++ VocableUITests/Tests/PresetCategoryTests.swift
@@ -17,23 +17,22 @@ class PresetCategoryTests: BaseTest {
         let categoryIdentifier = CategoryTitleCellIdentifier(CategoryIdentifier.general).identifier
         
         //Rename the preset category
-        settingsScreen.navigateToSettingsCategoryScreen()
-        settingsScreen.openCategorySettings(category: categoryName)
-        settingsScreen.renameCategoryButton.tap()
-        keyboardScreen.typeText(nameSuffix)
-        keyboardScreen.checkmarkAddButton.tap()
-        XCTAssertEqual(settingsScreen.categoryDetailsTitle.label, renamedCategory)
+        SettingsScreen.navigateToSettingsCategoryScreen()
+        SettingsScreen.openCategorySettings(category: categoryName)
+        SettingsScreen.renameCategoryButton.tap()
+        KeyboardScreen.typeText(nameSuffix)
+        KeyboardScreen.checkmarkAddButton.tap()
+        XCTAssertEqual(SettingsScreen.categoryDetailsTitle.label, renamedCategory)
         
-        settingsScreen.leaveCategoriesButton.tap()
-        XCTAssertTrue(settingsScreen.doesCategoryExist(renamedCategory))
+        SettingsScreen.navBarBackButton.tap()
+        XCTAssertTrue(SettingsScreen.doesCategoryExist(renamedCategory))
         
         // Return to the main screen
-        settingsScreen.leaveCategoriesButton.tap()
-        settingsScreen.exitSettingsButton.tap()
+        CustomCategoriesScreen.returnToMainScreenFromCategoriesList()
         
         // Confirm that the category is renamed from main screen
-        mainScreen.locateAndSelectDestinationCategory(.general)
-        XCTAssertEqual(mainScreen.selectedCategoryCell.identifier, categoryIdentifier)
+        MainScreen.locateAndSelectDestinationCategory(.general)
+        XCTAssertEqual(MainScreen.selectedCategoryCell.identifier, categoryIdentifier)
         //XCUIApplication().cells.staticTexts["Generaltest"].label
     }
     
@@ -42,21 +41,20 @@ class PresetCategoryTests: BaseTest {
         let categoryIdentifier = CategoryTitleCellIdentifier(CategoryIdentifier.environment).identifier
 
         //Remove the preset category
-        settingsScreen.navigateToSettingsCategoryScreen()
-        settingsScreen.openCategorySettings(category: categoryName)
-        settingsScreen.removeCategoryButton.tap()
-        settingsScreen.alertRemoveButton.tap()
-        XCTAssertFalse(settingsScreen.doesCategoryExist(categoryName))
+        SettingsScreen.navigateToSettingsCategoryScreen()
+        SettingsScreen.openCategorySettings(category: categoryName)
+        SettingsScreen.removeCategoryButton.tap()
+        SettingsScreen.alertRemoveButton.tap()
+        XCTAssertFalse(SettingsScreen.doesCategoryExist(categoryName))
         
         // Return to the main screen
-        settingsScreen.leaveCategoriesButton.tap()
-        settingsScreen.exitSettingsButton.tap()
+        CustomCategoriesScreen.returnToMainScreenFromCategoriesList()
         
         // Confirm that the category is no longer accessible from main screen
         for category in PresetCategories().list {
             // If we come across the category we expect to be removed, fail the test. Otherwise the test will pass
-            mainScreen.locateAndSelectDestinationCategory(category.categoryIdentifier)
-            if (mainScreen.selectedCategoryCell.identifier == categoryIdentifier) {
+            MainScreen.locateAndSelectDestinationCategory(category.categoryIdentifier)
+            if (MainScreen.selectedCategoryCell.identifier == categoryIdentifier) {
                 XCTFail("The category with identifier, '\(categoryIdentifier)', was not removed as expected.")
             }
         }
@@ -65,33 +63,9 @@ class PresetCategoryTests: BaseTest {
     func testShowHideButtonIsDisabledForMySayingsCategory() {
         let categoryName = "My Sayings"
        
-        settingsScreen.navigateToSettingsCategoryScreen()
-        settingsScreen.openCategorySettings(category: categoryName)
-        XCTAssertFalse(settingsScreen.showCategoryButton.isEnabled)
+        SettingsScreen.navigateToSettingsCategoryScreen()
+        SettingsScreen.openCategorySettings(category: categoryName)
+        XCTAssertFalse(SettingsScreen.showCategoryButton.isEnabled)
     }
-    
-    func testAddDuplicatePhrasesToMySayings() {
-        let testPhrase = "Test"
-        
-        mainScreen.keyboardNavButton.tap()
-        keyboardScreen.typeText(testPhrase)
-        keyboardScreen.favoriteButton.tap()
-        keyboardScreen.dismissKeyboardButton.tap()
-       
-        mainScreen.locateAndSelectDestinationCategory(.mySayings)
-        XCTAssertTrue(mainScreen.locatePhraseCell(phrase: testPhrase).exists, "Expected the first phrase \(testPhrase) to be added to and displayed in 'My Sayings'")
-
-        // Add the same phrase again to the My Sayings
-        mainScreen.addPhraseLabel.tap()
-        keyboardScreen.typeText(testPhrase)
-        keyboardScreen.checkmarkAddButton.tap()
-        keyboardScreen.createDuplicateButton.tap()
-        
-        // Assert that now we have two cells containing the same phrase
-        let phrasePredicate = NSPredicate(format: "label MATCHES %@", testPhrase)
-        let phraseQuery = XCUIApplication().staticTexts.containing(phrasePredicate)
-        phraseQuery.element.waitForExistence(timeout: 2)
-        XCTAssertEqual(phraseQuery.count, 2, "Expected both phrases to be presentin 'My Sayings'")
-    }
-    
+     
 }
