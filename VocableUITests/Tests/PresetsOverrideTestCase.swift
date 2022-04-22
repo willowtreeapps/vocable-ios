cad11364038c87c90039631084023c2a3b86380b
Chris Stroud <chris.stroud@willowtreeapps.com>
2022-04-22 15:31:37 -0400

    [587] Add example test case for overridding presets

---
+++ VocableUITests/Tests/PresetsOverrideTestCase.swift
@@ -0,0 +1,40 @@
+//
+//  PresetsOverrideTestCase.swift
+//  VocableUITests
+//
+//  Created by Chris Stroud on 4/22/22.
+//  Copyright © 2022 WillowTree. All rights reserved.
+//
+
+import XCTest
+
+class PresetsOverrideTestCase: XCTestCase {
+
+    override class func setUp() {
+        super.setUp()
+
+        let app = XCUIApplication()
+        app.launchArguments = ["resetAppDataOnLaunch"]
+        Presets {
+            Category("Custom Basic Needs") {
+                Phrase("Test Phrase")
+                Phrase("Another Phrase")
+                Phrase(id: "specific_id", "With some text")
+                Phrase(id: "another_id", languageCode: "es", "No sé donde estoy")
+            }
+            Category("Another Category") {
+                // Empty
+            }
+        }.encoded(to: app)
+
+        app.launch()
+    }
+
+    func testCustomPresets() throws {
+
+        // Use those custom presets!
+
+        // Uncomment if you want to play with the categories on the simulator
+        // Thread.sleep(until: .distantFuture)
+    }
+}
