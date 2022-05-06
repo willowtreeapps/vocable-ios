27fd883b1358522e051f1751d7b5cf3d8ecce45a
rudysalas <rhsalas3@gmail.com>
2022-05-06 17:28:11 -0400

    Created new helper function to navigate to a category directly from the Main Screen

--- VocableUITests/Tests/PaginationBaseTest.swift
+++ VocableUITests/Tests/PaginationBaseTest.swift
@@ -1,42 +1,67 @@
 //
-//  PaginationBaseTest.swift
+//  CustomCategoriesBaseTest.swift
 //  VocableUITests
 //
-//  Created by Rudy Salas on 5/6/22.
+//  Created by Rudy Salas and Canan Arikan on 5/06/22.
 //  Copyright © 2022 WillowTree. All rights reserved.
 //
 
 import XCTest
 
 class PaginationBaseTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+    
+    var categoryOne: Category {
+        Category(id: "first_category", "Category 1") {
+            Phrase("Please help")
+            Phrase("Hello")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
         }
     }
+    
+    var categoryTwo: Category {
+        Category(id: "second_category", "Category 2") {
+            Phrase("Please help")
+            Phrase("Hello")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+        }
+    }
+    
+    var categoryThree: Category {
+        Category(id: "third_category", "Category 3") {
+            Phrase("Please help")
+            Phrase("Hello")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+            Phrase("I need a blanket")
+        }
+    }
+    
+    override func setUp() {
+        let app = XCUIApplication()
+        app.configure {
+            Arguments(.resetAppDataOnLaunch, .enableListeningMode)
+            Environment(.overridePresets) {
+                Presets {
+                    categoryOne
+                    categoryTwo
+                    categoryThree
+                }
+            }
+        }
+        app.launch()
+    }
+    
 }
