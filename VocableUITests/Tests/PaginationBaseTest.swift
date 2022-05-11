f6ef3df66b29c7ffb0aa64a45b617aaacc4f10d3
rudysalas <rhsalas3@gmail.com>
2022-05-11 15:34:50 -0400

    Added init to PresetBuilders::Category which allows us to pass in an array of phrases; refactored PaginationBaseTest to build array of the same phrase instead of copy/paste each phrase

--- VocableUITests/Tests/PaginationBaseTest.swift
+++ VocableUITests/Tests/PaginationBaseTest.swift
@@ -9,39 +9,16 @@
 import XCTest
 
 class PaginationBaseTest: XCTestCase {
-    
-    let categoryOne = Category(id: "first_category", "Category 1") {
-        Phrase("Please help")
-        Phrase("Hello")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
+
+    static private func phrases(count: Int) -> [Phrase] {
+        (1...count).map { _ in
+            Phrase("Hello")
+        }
     }
     
-    let categoryTwo = Category(id: "second_category", "Category 2") {
-        Phrase("Please help")
-        Phrase("Hello")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-    }
-    
-    let categoryThree = Category(id: "third_category", "Category 3") {
-        Phrase("Please help")
-        Phrase("Hello")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-        Phrase("I need a blanket")
-    }
+    let categoryOne = Category(id: "first_category", "Category 1", phrases: PaginationBaseTest.phrases(count: 8))
+    let categoryTwo = Category(id: "second_category", "Category 2", phrases: PaginationBaseTest.phrases(count: 9))
+    let categoryThree = Category(id: "third_category", "Category 3", phrases: PaginationBaseTest.phrases(count: 7))
     
     override func setUp() {
         let app = XCUIApplication()
